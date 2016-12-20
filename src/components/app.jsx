dcc48c36e321490f47b8a9f566c32ab8a529e29e
Martin Laine <martin.laine@gmail.com>
2016-12-20 18:02:38 +0000

    Basic layout

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -6,10 +6,12 @@ import { refreshTracks } from '../actions';
 import TrackList from './track_list';
 
 const App = ({ tracks, onRefreshClick }) => (
-  <div>
-    <h1>Cloud mixes</h1>
-    <button onClick={onRefreshClick}>Refresh</button>
-    <TrackList tracks={tracks} />
+  <div className="u-flex u-flex--vertical">
+    <header className="u-flex__panel u-flex u-flex--horizontal toolbar">
+      <h1 className="u-flex__panel u-flex__panel--grow">Cloud mixes</h1>
+      <button onClick={onRefreshClick}>Refresh</button>
+    </header>
+    <TrackList tracks={tracks} className="u-flex__panel u-flex__panel--grow" />
   </div>
 );
 
--- src/components/track_list.jsx
+++ src/components/track_list.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import classnames from 'classnames';
 
 import Track from './track';
 
-const TrackList = ({ tracks }) => (
-  <div>
+const TrackList = ({ tracks, className }) => (
+  <div className={classnames("track-list", className)}>
     {tracks.map(track => (
       <Track key={track.id} track={track} />
     ))}
