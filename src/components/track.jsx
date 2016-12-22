18f8500ab5bd67a2e9bb716016a2252cb08c356e
Martin Laine <martin.laine@gmail.com>
2016-12-22 02:09:46 +0000

    clean up track list

--- src/components/track.jsx
+++ src/components/track.jsx
@@ -4,9 +4,10 @@ import { formatDuration } from '../utils';
 
 const Track = ({ track, onSelect }) => (
   <div className="track-list__item track u-flex u-flex--horizontal">
-    <div className="track__artwork u-flex__panel"
+    <div className="track__button  u-flex__panel"
       onClick={() => onSelect(track.id)}>
-      <img src={track.artwork_url} />
+      <img className="track__artwork" src={track.artwork_url} />
+      <i className="material-icons md-24 track__button-icon">play_arrow</i>
     </div>
     <div className="track__meta u-flex__panel u-flex__panel--grow">
       <span className="track__meta-item track__title">{track.title}</span>
