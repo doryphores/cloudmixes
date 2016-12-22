ee10d4a7920a4b3a1f79c92a6822f0e17e96046a
Martin Laine <martin.laine@gmail.com>
2016-12-22 03:19:42 +0000

    Add context menu and quit action

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const menubar = require('menubar');
+const { Menu } = require('electron');
 
 const mb = menubar({
   dir: process.cwd() + '/app',
   transparent: true,
   skipTaskbar: true,
   width: 500,
-  height: 400
+  height: 400,
+  tooltip: 'Cloud mixes'
 });
 
+const contextMenu = Menu.buildFromTemplate([
+  {
+    role: 'quit'
+  }
+]);
+
 mb.on('ready', () => {
+  mb.tray.setContextMenu(contextMenu);
   mb.showWindow();
 });
